87e8cb945ba02a14c01ce337ea3bc60d02a9650e
RandikaVS <83448005+RandikaVS@users.noreply.github.com>
2022-09-18 13:46:52 +0530

    #60 Create customer manage

--- admin/view-customer.php
+++ admin/view-customer.php
@@ -1,3 +1,6 @@
+<?php
+include_once(dirname(__FILE__) . '../../model/include.php');
+?>
 <html>
 
 <head>
@@ -49,53 +52,39 @@
                                     </thead>
                                     <tbody>
                                         <?php
+                                            $Customer = new Customer();
+                                            foreach ($Customer->getAllCustomers() as $customer) {
 
-                                        $serverName = "localhost";
-                                        $userName = "root";
-                                        $password = "";
-                                        $databaseName = "willudainn";
+                                                if($customer['isActive']==0){
+                                                    $status="<span style='color:red;'>Blocked</span>";
+                                                    $blockBtn = "Unblock";
+                                                }
+                                                else{
+                                                    $status="<span style='color:green;'>Active</span>";
+                                                    $blockBtn = "Block";
+                                                }
+                                                ?>
 
-                                        $connection = new mysqli($serverName, $userName, $password, $databaseName);
+                                            <td><?php echo $customer['id'] ?></td>
 
-                                        if ($connection->connect_error) {
-                                            die("Connection failed" . $connection->connect_error);
-                                        }
+                                            <td><?php echo $customer['name'] ?></td>
 
-                                        $sql = "SELECT * FROM customer";
-                                        $result = $connection->query($sql);
+                                            <td><?php echo $customer['email'] ?></td>
 
-                                        if (!$result) {
-                                            die("Invalid query" . $connection->error);
-                                        }
+                                            <td><?php echo $customer['residance'] ?></td>
 
+                                            <td><?php echo $customer['number'] ?></td>
 
-                                        while ($row = $result->fetch_assoc()) {
+                                            <td><?php echo $customer['createdAt'] ?></td>
 
-                                            $id = $row['id'];
-                                            $name = $row['name'];
-                                            // $email = $row['email'];
-                                        ?>
+                                            <td><?php echo $status ?></td>
 
-                                            <td><?php echo $row['id'] ?></td>
-
-                                            <td><?php echo $row['name'] ?></td>
-
-                                            <td><?php echo $row['email'] ?></td>
-
-                                            <td><?php echo $row['residance'] ?></td>
-
-                                            <td><?php echo $row['number'] ?></td>
-
-                                            <td><?php echo $row['createdAt'] ?></td>
-
-                                            <td><?php echo $row['isActive'] ?></td>
-
-                                            <td><?php echo $row['lastLogin'] ?></td>
+                                            <td><?php echo $customer['lastLogin'] ?></td>
 
                                             <td>
-                                                <a class='btn btn-danger' role='button' id='block'>Block</a>
+                                                <a class='btn btn-danger block-customer' data-id="<?=$customer['id'];?>" status="<?=$customer['isActive'];?>" role='button' id='block'><?php echo $blockBtn?></a>
                                                 <span>
-                                                    <a class='btn btn-primary' role='button' href="edit-customer.php">Edit</a>
+                                                    <a class='btn btn-primary' role='button' href="edit-customer.php?id=<?=$customer['id'];?>">Edit</a>
                                                 </span>
                                             </td>
                                             </tr>
@@ -131,7 +120,6 @@
 
 
     <script src="js/app.js"></script>
-    <script src="js/ajax/service.js" type="text/javascript"></script>
     <script>
         document.addEventListener("DOMContentLoaded", function() {
             // Datatables basic
@@ -244,7 +232,7 @@
     </script>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-sweetalert/1.0.1/sweetalert.min.js" integrity="sha512-MqEDqB7me8klOYxXXQlB4LaNf9V9S0+sG1i8LtPOYmHqICuEZ9ZLbyV3qIfADg2UJcLyCm4fawNiFvnYbcBJ1w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
-    <script type="text/javascript" src="js/ajax/customerMng.js"></script>
+    <script type="text/javascript" src="validations/js/customerMng.js"></script>
 
 </body>
 
